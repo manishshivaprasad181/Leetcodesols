f26e347f65727b484091e3513898a56df1c957aa
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-04-25 23:12:44 +0530

    Danger Problem

---
+++ climbStairs.java
@@ -0,0 +1,17 @@
+class climbStairs{
+    public int climbStairs(int n) {
+      if(n==1)
+      return 1;
+      if(n==2)
+      return 2;
+       int[] f=new int[n+1];
+       f[0]=0;
+       f[1]=1;
+       f[2]=2;
+       for(int i=3;i<n+1;i++)
+       {
+           f[i]=f[i-1]+f[i-2];
+       } 
+       return f[n];
+    }
+}
