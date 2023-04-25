474de1f3d5661143f283c2765bd59122c0d1774d
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-04-26 00:51:07 +0530

    Danger problem

---
+++ maxProfit.java
@@ -0,0 +1,14 @@
+class maxProfit{
+    public int maxProfit(int[] prices) {
+        int min=Integer.MAX_VALUE;
+        int maxprofit=0;
+        for(int i=0;i<prices.length;i++)
+        {
+            if(min>prices[i])
+            min=prices[i];
+            else if(prices[i]-min>maxprofit)
+            maxprofit=prices[i]-min;
+        }
+        return maxprofit;
+    }
+}
