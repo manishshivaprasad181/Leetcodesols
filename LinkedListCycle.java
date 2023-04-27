7c582e5c2ab0c7a5ceb745696d43d55be4daab83
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-04-27 08:43:09 +0530

    PARTIAL Danger Problem Apr 26

---
+++ LinkedListCycle.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        /*Set<ListNode> visited=new HashSet<>();
+        while(head!=null)
+        {
+            if(visited.contains(head))
+            return true;
+            visited.add(head);
+            head=head.next;
+        }
+        return false;*/
+        /*Optimized code*/
+        if(head==null)
+        return false;
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(slow!=fast){
+            if(fast==null||fast.next==null)
+            return false;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return true;
+    }
+}
