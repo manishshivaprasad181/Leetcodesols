53be120c8e00580a8fb38e588cc132e2e17c44dc
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-05-01 15:47:23 +0530

    May 1

---
+++ RemoveElements.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode sentinel=new ListNode(0);
+        sentinel.next=head;
+        ListNode curr=head,prev=sentinel;
+        while(curr!=null)
+        {
+            if(curr.val==val)prev.next=curr.next;
+            else
+            prev=curr;
+            curr=curr.next;
+        }
+    
+        return sentinel.next;
+    }
+}
