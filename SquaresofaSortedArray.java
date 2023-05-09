ca0c84b3d07d6a0300924796c54b579181a9cdb2
manishshivaprasad181 <114031233+manishshivaprasad181@users.noreply.github.com>
2023-05-09 22:29:59 +0530

    May 9

---
+++ SquaresofaSortedArray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int left=0;
+        int right=nums.length-1;
+        int k=nums.length-1;
+        int []result=new int[k+1];
+        while(left<=right)
+        {
+            if(Math.abs(nums[left])>=Math.abs(nums[right]))
+            {
+                result[k--]=nums[left]*nums[left];
+                left++;
+            }
+            else
+            {
+                result[k--]=nums[right]*nums[right];
+                right--;
+            }
+        }
+    return result;
+
+    }
+}
